c3904923889019332a18873c10ec90e88e23f3ed
Stephen Mayeux <stephenmayeux@gmail.com>
2017-11-29 20:50:23 -0600

    add new posts

--- public/main.js
+++ public/main.js
@@ -7,31 +7,60 @@ $(document).ready(function() {
     storageBucket: "reddit-clone-test-fd103.appspot.com",
     messagingSenderId: "939272960776"
   };
+  var DAY_IN_MS = 86400000;
 
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  database.ref('/items').once('value').then(function(snapshot) {
-    var results = snapshot.val();
-    var counter = 1;
-    for (var id in results) {
-      buildItemElement(results[id], counter);
-      counter++;
-    }
-  })
+  database.ref('items')
+    .orderByChild('createdAt')
+    .startAt(Date.now() - DAY_IN_MS)
+    .on('child_added', function(snapshot) {
+      if ($('#loadingMsg').is(':visible')) {
+        $('#loadingMsg').hide();
+      }
+      var result = snapshot.val();
+      result.id = snapshot.key;
+      buildItemElement(result);
+    })
 
-  function buildItemElement(item, index) {
+  function buildItemElement(item) {
     var $template = $('#content-template').clone()
     var newItem = $template.prop('content')
 
     // Inject data into template elements
-    $(newItem).find('.content-number').text(index)
     $(newItem).find('.content-title').text(item.title)
+    $(newItem).find('.arrow').attr('id', item.id);
     $(newItem).find('.votes').text(item.votes)
-    $(newItem).find('.content-link').attr('href', item.link)
-    $(newItem).find('.content-link').attr('target', '_blank')
+    $(newItem).find('.content-link').attr('href', item.link).attr('target', '_blank')
     $(newItem).find('.content-meta').text(item.user + ' posted ' + moment(item.createdAt).fromNow())
 
-    $('#list').append(newItem)
+    $('#list').prepend(newItem)
   }
+
+  $('#sharePost').on('click', function() {
+    var link = $('#inputURL').val();
+    var title = $('#inputTitle').val();
+    var user = $('#inputUser').val();
+    var createdAt = Date.now();
+    var votes = 0;
+
+    var data = {
+      link: link,
+      title: title,
+      user: user,
+      createdAt: createdAt,
+      votes: votes
+    }
+
+    var itemsListRef = database.ref('items');
+    var newItemRef = itemsListRef.push(data, function(err) {
+      if (err) {
+        console.error('Error saving to firebase', err);
+      } else {
+        console.log('Success saving to firebase!')
+        $('#addPost').modal('hide');
+      }
+    });
+  })
 });
